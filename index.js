12beea620667b984782091ff455fa350fc3fe82d
Vicente Fuentes <709849@cognizant.com>
2018-08-24 12:13:59 +0200

    making app optional

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const getLogger = () => {
 };
 
 
-const initializeLogger = (app, fileConf = null, consoleConf = null) => {
-    app.use(httpContext.middleware);
+const initializeLogger = (app = null, fileConf = null, consoleConf = null) => {
+		if(app) {
+				app.use(httpContext.middleware);
 
-    // Run the context for each request. Assign a unique identifier to each request
-    app.use(function(req, res, next) {
-        httpContext.set('reqId', uuidv1());
-        next();
-    });
+				// Run the context for each request. Assign a unique identifier to each request
+				app.use(function(req, res, next) {
+						httpContext.set('reqId', uuidv1());
+						next();
+				});
+		}
 
     logger = loggerBuilder.initializeLogger(fileConf, consoleConf);
 };
